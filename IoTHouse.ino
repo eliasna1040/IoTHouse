20b11149f152d6cf1e0b2d22eb72c006752191b9
c98101ena <elias.andersen@hessel.dk>
2023-06-08 13:30:14 +0200

    added menu in serial

--- IoTHouse.ino
+++ IoTHouse.ino
@@ -1,9 +1,80 @@
+#include "EasyButton.h"
+#define BUTTON_1_PIN 5
+#define BUTTON_2_PIN 6
+int debounce = 50;
+int menuInput = 0;
+int menuCount = 1;
+bool selected = false;
+
+EasyButton button_1(BUTTON_1_PIN, debounce);
+EasyButton button_2(BUTTON_2_PIN, debounce);
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
 
+  button_1.begin();
+  button_1.enableInterrupt(buttonISR_1);
+  button_1.onPressed(changeMenu);
+
+  button_2.begin();
+  button_2.enableInterrupt(buttonISR_2);
+  button_2.onPressed(selectEvent);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  switch (menuInput) {
+   case 1:
+    time();
+    break;
+   case 2:
+    temperature();
+    break;
+   default:
+    printMenu();
+    break;
+  }
+  delay(2000);
+}
 
+
+void printMenu(){
+  Serial.println("1. Time");
+  Serial.println("2. Temperature");
+  Serial.println(menuCount);
+}
+
+void temperature(){
+  Serial.print("temp = ");
+  Serial.println(menuInput);
+}
+
+void time(){
+  Serial.println("nothing here");
+}
+
+void buttonISR_1(){
+  button_1.read();
+}
+
+void buttonISR_2(){
+  button_2.read();
+}
+
+void selectEvent(){
+  menuInput = menuCount;
+}
+
+void changeMenu(){
+  if (menuInput > 0) {
+    menuInput = 0;
+  }
+  else {  
+    if (menuCount == 2) {
+      menuCount = 1;
+    }
+    else {
+      menuCount++;
+    }
+  }
 }
